398328898ce4ee09054f750a07a2c261b91d02e2
Isabela Schurhaus Hyeda Buitoni <isabelahyeda@gmail.com>
2022-12-07 09:38:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -27,21 +27,21 @@ function calcular() {
 	const imc = (valorPeso() / Math.pow(altura,2)).toFixed(2)
 
 	if(imc < 16) {
-    	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau III.`
-    } else if(imc >= 16 && imc < 17) {
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau II.`
-    } else if(imc >= 17 && imc < 18.50){
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau I.`
-    } else if(imc >= 18.50 && imc < 25){
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Peso adequado.`
-    } else if(imc >= 25 && imc < 30) {
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Sobrepeso.`
-    } else if(imc >= 30 && imc < 35) {
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau I.`
-    } else if(imc >= 35 && imc < 40) {
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau II.`
-    } else {
-        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau III.`
+    		resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau III.`
+    	} else if(imc >= 16 && imc < 17) {
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau II.`
+    	} else if(imc >= 17 && imc < 18.50){
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau I.`
+    	} else if(imc >= 18.50 && imc < 25){
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Peso adequado.`
+    	} else if(imc >= 25 && imc < 30) {
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Sobrepeso.`
+    	} else if(imc >= 30 && imc < 35) {
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau I.`
+    	} else if(imc >= 35 && imc < 40) {
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau II.`
+    	} else {
+        	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau III.`
    	}
 }
 
