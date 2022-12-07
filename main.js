960315f31fff1ee5871986069b94be87c183682a
Isabela Hyeda <isabelahyeda@gmail.com>
2022-12-07 09:25:18 -0300

    melhorias na validação dos campos

--- main.js
+++ main.js
@@ -1,55 +1,54 @@
 function valorPeso() {
-	let peso = document.getElementById('peso').value
-	peso = parseFloat(peso)
-	
-	return peso
+	return (parseFloat (document.getElementById('peso').value) || 0)
 }
 
 function valorAltura() {
-	let altura = document.getElementById('altura').value
-	altura = parseFloat(altura)
+	let altura = (parseFloat(document.getElementById('altura').value) || 0)
 
-	return altura
+	if (!Number.isInteger(altura)) {
+		altura *= 100
+		return altura
+	} else {
+		return altura
+	}
 }
 
 function calcular() {
 
-	let resultado = document.getElementById('resultado')
+	const resultado = document.getElementById('resultado')
 
-	if (!Number.isInteger(valorAltura())) {
-		resultado.innerHTML = 'Por favor, digite a altura em centímetros, não use ponto ou vírgula.'
+	if (valorPeso() == '' || valorAltura() == '') {
+		resultado.textContent = 'Por favor, informe todos os campos acima.'
 		return
 	}
 
-	let altura = valorAltura() / 100
+	const altura = valorAltura() / 100
 
-	let imc = valorPeso() / Math.pow(altura,2)
-
-	imc = imc.toFixed(2)
+	const imc = (valorPeso() / Math.pow(altura,2)).toFixed(2)
 
 	if(imc < 16) {
-    		resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Magreza grau III.`
-   	 } else if(imc >= 16 && imc < 17) {
-           	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Magreza grau II.`
-    	 } else if(imc >= 17 && imc < 18.50){
-            	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Magreza grau I.`
-    	 } else if(imc >= 18.50 && imc < 25){
-            	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Peso adequado.`
-    	 } else if(imc >= 25 && imc < 30) {
-            	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Sobrepeso.`
-    	 } else if(imc >= 30 && imc < 35) {
-            	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau I.`
-    	 } else if(imc >= 35 && imc < 40) {
-            	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau II.`
-    	 } else {
-            	resultado.innerHTML = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau III.`
-   	 }
+    	resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau III.`
+    } else if(imc >= 16 && imc < 17) {
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau II.`
+    } else if(imc >= 17 && imc < 18.50){
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Magreza grau I.`
+    } else if(imc >= 18.50 && imc < 25){
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Peso adequado.`
+    } else if(imc >= 25 && imc < 30) {
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Sobrepeso.`
+    } else if(imc >= 30 && imc < 35) {
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau I.`
+    } else if(imc >= 35 && imc < 40) {
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau II.`
+    } else {
+        resultado.textContent = `Seu IMC é ${imc}, sendo classificado como: Obesidade grau III.`
+   	}
 }
 
 function limpar() {
 	document.getElementById('peso').value=''
 	document.getElementById('altura').value=''
-	document.getElementById('resultado').value= resultado.innerHTML = ''
+	document.getElementById('resultado').value= resultado.textContent = ''
 }
 
 function abrirPagina() {
